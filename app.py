bff11021af0328c1bbcd58d88672ffba1e1bc9f2
Arifur Rahman <97516883+Arif-Devs@users.noreply.github.com>
2026-02-02 20:59:07 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
+
 def create_app():
-    app = Flask(__name__)
-    @app.route('/')
-   
     
+    app = Flask(__name__)
+    @app.route('/')  
     def home():
         print("hi hello")
         return 'Hi Sudhanshu'
